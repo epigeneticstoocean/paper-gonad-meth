1f21d0d68f9caf3ca0e01a0019a4e5e7003ced7a
sr320 <roberts.sbr@gmail.com>
2019-03-26 13:45:28 -0700

    removed hyak sp header

--- code/bismark.sh
+++ code/bismark.sh
@@ -1,43 +1,7 @@
-#!/bin/bash
-
-## Job Name
-
-#SBATCH --job-name=20181012_bismark_yaamini
-
-## Allocation Definition
-
-#SBATCH --account=srlab
-
-#SBATCH --partition=srlab
-
-## Resources
-
-## Nodes
-
-#SBATCH --nodes=1
-
-## Walltime (days-hours:minutes:seconds format)
-
-#SBATCH --time=30-00:00:00
-
-## Memory per node
-
-#SBATCH --mem=120G
-
-##turn on e-mail notification
-
-#SBATCH --mail-type=ALL
-
-#SBATCH --mail-user=yaaminiv@uw.edu
-
-## Specify the working directory for this job
-
-#SBATCH --workdir=/gscratch/scrubbed/yaamini/analyses/Virginica-MBD/2018-10-31-Virginica-Bismark-Revised-Parameters-Samtools
 
 
 
-#Add Directories and Programs to Path
-
+# Add Directories and Programs to Path
 
 
 bismark_dir="/gscratch/srlab/programs/Bismark-0.19.0"
@@ -52,13 +16,13 @@ samtools_dir="/gscratch/srlab/programs/samtools-1.9/"
 
 
 
-#Genome preparation
+# Genome preparation
 
-#YRV prepared the bisulfite genome on hummingbird: https://github.com/RobertsLab/project-virginica-oa/blob/master/notebooks/2018-04-27-Gonad-Methylation-Bismark.ipynb
+# YRV prepared the bisulfite genome on hummingbird: https://github.com/RobertsLab/project-virginica-oa/blob/master/notebooks/2018-04-27-Gonad-Methylation-Bismark.ipynb
 
 
 
-#Alignment
+# Alignment
 
 
 
