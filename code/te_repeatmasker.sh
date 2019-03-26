cf6c6b1f1da9ecfb8bdbe896381bfacbd4c064c8
Sam White <samuel.j.white@gmail.com>
2019-03-26 14:56:20 -0700

    reorder file; add error message if diff fails

--- code/te_repeatmasker.sh
+++ code/te_repeatmasker.sh
@@ -5,8 +5,17 @@
 wd=""
 
 # FastA URL
-fasta_url="wget http://owl.fish.washington.edu/halfshell/genomic-databank/Cvirginica_v300.fa"
+fasta_url="http://owl.fish.washington.edu/halfshell/genomic-databank/Cvirginica_v300.fa"
 fasta="Cvirginica_v300.fa"
 fasta_md5="f9135e323583dc77fc726e9df2677a32"
-dl_md5=$(md5sum ${wd}/${fasta} | awk '{ print $1 }')
-diff <(echo "$md5") <(echo "$dl_md5")
+
+# Download file
+wget "${fasta_url}" \
+--directory-prefix="${wd}"
+
+# Generate checksum
+dl_md5=$(md5sum "${wd}"/"${fasta}" | awk '{ print $1 }')
+
+#Compare cheksums
+diff <(echo "${fasta_md5}") <(echo "${dl_md5}") \
+|| echo "Checksums do not match. Try re-downloading file and then re-running script." exit 1
